2895f18f222960b65eccb211d2d337dd58dca03b
Rutuja Pawar <rp316523@gmail.com>
2025-05-30 23:22:58 +0530

    Positive no

---
+++ Positive Number/Positive Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Positive_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            if (a > 0)
+            {
+                Console.WriteLine("Number is Positive");
+            }
+            Console.ReadKey();
+        }
+    }
+}
