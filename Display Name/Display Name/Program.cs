4e0066f501247ad479d8271d29cbf1ed3485dd2f
Rutuja Pawar <rp316523@gmail.com>
2025-05-30 23:20:38 +0530

    Display Name

---
+++ Display Name/Display Name/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Display_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Rutuja Pawar";
+            if (name == "Rutuja Pawar") ;
+            {
+                Console.WriteLine("Rutuja Pawar");
+            }
+            Console.ReadKey();
+        }
+    }
+}
