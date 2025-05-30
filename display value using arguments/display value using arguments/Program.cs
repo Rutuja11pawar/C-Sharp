66c64eb419d7fc30aaa81b98f1289fc06cea7d86
Rutuja Pawar <rp316523@gmail.com>
2025-05-30 23:21:09 +0530

    value argument

---
+++ display value using arguments/display value using arguments/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace display_value_using_arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int argument1 = Convert.ToInt31(args[0]);
+            Console.WriteLine("Argument in interger is : " + argument1);
+
+            double argument2 = Convert.ToDouble(args[1]);
+            Console.WriteLine("Argument in double is : " + argument2);
+            Console.ReadKey();
+        }
+    }
+}
