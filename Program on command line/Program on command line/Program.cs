f37b03bc1f44e46176f18df90e5ce3194be0f87c
Rutuja Pawar <rp316523@gmail.com>
2025-05-30 23:23:37 +0530

    Command line argument

---
+++ Program on command line/Program on command line/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program_on_command_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Name is : " + args[0]);
+            Console.WriteLine("Last Name is : " + args[1]);
+            Console.ReadKey();
+        }
+    }
+}
