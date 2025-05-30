25ff46de06836afc0170fda11b2a9f3b48b755d6
Rutuja Pawar <rp316523@gmail.com>
2025-05-30 23:22:05 +0530

    Even odd

---
+++ Even Odd/Even Odd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Even_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 11;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("It is Even Number");
+            }
+            else
+            {
+                Console.WriteLine("It is Odd Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
